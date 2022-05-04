166a26eddc5dc654838583a33dff5d288b4d370c
liaofnelson <90040705+liaofnelson@users.noreply.github.com>
2022-05-04 14:45:37 +1000

    Add files via upload

---
+++ Demo04/src/FoodModel.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+
+/**
+ * @ClassName FoodModel
+ * @Description: TODO data processing model layer
+ * @Author: Fengyu Liao
+ */
+public class FoodModel {
+    /**
+     * Get all food information
+     * @return all food information
+     */
+    public ArrayList<Food> getAll() { return Global.FoodList; }
+
+    /**
+     * look up by food name
+     * @param FoodName
+     * @return results collection
+     */
+     public ArrayList<Food> getByFoodName(String FoodName){
+         ArrayList<Food> resList = new ArrayList<>();
+
+         for (Food food : Global.FoodList){
+             if (FoodName.equals(food.getFood()))
+                 resList.add(food);
+
+         }
+         return resList;
+     }
+
+}
