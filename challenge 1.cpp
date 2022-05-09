833cc60360a94a3bd62fe652fdd393ae06d0cf4d
liaofnelson <90040705+liaofnelson@users.noreply.github.com>
2022-05-09 21:11:21 +1000

    Update challenge 1.cpp

--- challenge 1.cpp
+++ challenge 1.cpp
@@ -1,19 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* fibonacci sequence function*/
+
 int fib(int n){
 	if (n <= 1)
     return n;
     return fib(n-1) + fib(n-2);
 }
 
+
 int main (){
-	int n,f,count=1,k;
-    cout<<"Enter index n: ";
+    int n,f,count=1,k; // f is input number; count is index; k is a number in fibonacci sequence
+    cout<<"Enter index n: "; // finding values according to the index
     cin>>n;
     cout <<n<<"th number in the fibonacci sequence is " <<fib(n);
-    cout<<"\nEnter a number: ";
+    cout<<"\nEnter a number: "; 
     cin>>f;
+	
+	
+/*
+if value is equal to the number in the fib sequence, the index will be shown,
+if the value is smaller than the number in the fib sequence, the closest index will be shown.
+*/
 while(1){
 	k=fib(count++);
 	if(k==f){
